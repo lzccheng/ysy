940e2a5a51b6746641978965756c6b5f1aab8d3f
lzccheng <1187350675@qq.com>
2019-12-10 14:16:25 +0800

    rem

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="content_box">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import url(./style/reset.scss);
+.content_box {
+  width: 10rem;
+  margin: 0 auto;
+}
+</style>
